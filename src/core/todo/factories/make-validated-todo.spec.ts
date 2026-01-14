9a88777df2ae4d1432eed63da95819f580fc8a71
¡Duque <atanaelleonardo@gmail.com>
2026-01-14 00:38:49 -0300

    feat: create make-validated-todo.ts factory function

---
+++ src/core/todo/factories/make-validated-todo.spec.ts
@@ -0,0 +1,5 @@
+describe("makeValidatedTodo (unit)", () => {
+  it("should be defined", () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ src/core/todo/factories/make-validated-todo.ts
@@ -0,0 +1,1 @@
+export function makeValidatedTodo(todo: any) {}
