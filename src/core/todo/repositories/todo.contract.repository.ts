c2a65d9de1727254c20dbb66bd95342e460be5e5
¡Duque <atanaelleonardo@gmail.com>
2026-01-16 01:15:35 -0300

    chore: define Todo repository interfaces for improved type safety

--- src/core/todo/repositories/todo.contract.repository.ts
+++ src/core/todo/repositories/todo.contract.repository.ts
@@ -1,3 +1,16 @@
+import type { Todo, TodoPresenter } from '../schemas/todo-contract';
+
 export interface FindAllTodosRepository {
-  findAll(): Promise<any[]>;
+  findAll(): Promise<Todo[]>;
 }
+
+export interface CreateTodoRepository {
+  create(todo: Todo): Promise<TodoPresenter>;
+}
+
+export interface DeleteTodoRepository {
+  remove(id: string): Promise<TodoPresenter>;
+}
+
+export interface TodoRepository
+  extends FindAllTodosRepository, CreateTodoRepository, DeleteTodoRepository {}
