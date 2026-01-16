a610b0720304c6293979a77052187634076615d2
¡Duque <atanaelleonardo@gmail.com>
2026-01-16 01:23:31 -0300

    feat: implement findAll method in DrizzleTodoRepository for fetching todos

--- src/core/todo/repositories/drizzle-todo.repository.ts
+++ src/core/todo/repositories/drizzle-todo.repository.ts
@@ -1,13 +1,29 @@
+import type { DrizzleDatabase } from '@/db';
 import type { Todo, TodoPresenter } from '../schemas/todo-contract';
 import type { TodoRepository } from './todo.contract.repository';
 
 export class DrizzleTodoRepository implements TodoRepository {
-  findAll(): Promise<Todo[]> {
-    throw new Error('Method not implemented.');
+  private readonly db: DrizzleDatabase;
+
+  constructor(db: DrizzleDatabase) {
+    this.db = db;
   }
+
+  async findAll(): Promise<Todo[]> {
+    const todos = await this.db.query.todo.findMany({
+      orderBy: (todo, { desc }) => [
+        desc(todo.createdAt),
+        desc(todo.description),
+      ],
+    });
+
+    return todos;
+  }
+
   create(todo: Todo): Promise<TodoPresenter> {
     throw new Error('Method not implemented.');
   }
+
   remove(id: string): Promise<TodoPresenter> {
     throw new Error('Method not implemented.');
   }
