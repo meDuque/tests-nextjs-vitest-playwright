35c8ecde08d1c075393a6d4460207af7bff70d6c
¡Duque <atanaelleonardo@gmail.com>
2026-01-20 18:23:31 -0300

    feat: add decorators and new story variants to `Button.stories.tsx`

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -3,14 +3,39 @@ import { Button } from '.';
 
 const meta = {
   component: Button,
+  decorators: [
+    (Story) => (
+      <div className="max-w-screen-md mx-auto p-12 flex items-center justify-center">
+        <Story />
+      </div>
+    ),
+  ],
 } satisfies Meta<typeof Button>;
 
 export default meta;
 
 type Story = StoryObj<typeof Button>;
 
-export const Playground: Story = {
+export const Default: Story = {
   args: {
-    children: 'Texto do botão',
+    children: 'Olá mundo',
+    variant: 'default',
+    size: 'lg',
+  },
+};
+
+export const Danger: Story = {
+  args: {
+    children: 'Olá mundo',
+    variant: 'danger',
+    size: 'lg',
+  },
+};
+
+export const Ghost: Story = {
+  args: {
+    children: 'Olá mundo',
+    variant: 'ghost',
+    size: 'lg',
   },
 };
