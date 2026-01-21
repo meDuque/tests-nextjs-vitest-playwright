0010b8b72aff8bae72b825b7476e617961907169
¡Duque <atanaelleonardo@gmail.com>
2026-01-20 22:59:11 -0300

    test: update Button spec to use SIZE_MD_CLASSES instead of SIZE_MEDIUM_CLASSES and add DISABLED_CLASSES

--- src/components/Button/Button.spec.tsx
+++ src/components/Button/Button.spec.tsx
@@ -11,12 +11,14 @@ import { Button } from '.';
 const VARIANT_DEFAULT_CLASSES = 'bg-blue-600 hover:bg-blue-700 text-blue-100';
 const VARIANT_DANGER_CLASSES = 'bg-red-600 hover:bg-red-700 text-red-100';
 const VARIANT_GHOST_CLASSES = 'bg-slate-300 hover:bg-slate-400 text-slate-950';
-const SIZE_MEDIUM_CLASSES =
+const SIZE_MD_CLASSES =
   'text-base/tight py-2 px-4 rounded-md [&_svg]:w-4 [&_svg]:h-4 gap-2';
 const SIZE_SM_CLASSES =
   'text-xs/tight py-1 px-2 rounded-sm [&_svg]:w-3 [&_svg]:h-3 gap-1';
 const SIZE_LG_CLASSES =
   'text-lg/tight py-4 px-6 rounded-lg [&_svg]:w-5 [&_svg]:h-5 gap-3';
+const DISABLED_CLASSES =
+  'disabled:bg-slate-200 disabled:text-slate-400 disabled:cursor-not-allowed';
 
 describe('<Button />', () => {
   describe('props padrão e JSX', () => {
@@ -28,7 +30,7 @@ describe('<Button />', () => {
       });
 
       expect(button).toHaveClass(VARIANT_DEFAULT_CLASSES);
-      expect(button).toHaveClass(SIZE_MEDIUM_CLASSES);
+      expect(button).toHaveClass(SIZE_MD_CLASSES);
 
       // expect(button).toMatchSnapshot();
     });
@@ -111,7 +113,7 @@ describe('<Button />', () => {
 
       const button = screen.getByTitle(/o botão/i);
 
-      expect(button).toHaveClass(SIZE_MEDIUM_CLASSES);
+      expect(button).toHaveClass(SIZE_MD_CLASSES);
     });
 
     test('tamanho lg deve ser grande', async () => {
@@ -127,7 +129,16 @@ describe('<Button />', () => {
     });
   });
 
-  // describe('disabled', () => {
-  //   test('classes para estado desativado estão corretas', async () => {});
-  // });
+  describe('disabled', () => {
+    test('classes para estado desativado estão corretas', async () => {
+      render(<Button disabled>Enviar formulário</Button>);
+
+      const button = screen.getByRole('button', {
+        name: /enviar formulário/i,
+      });
+
+      expect(button).toHaveClass(DISABLED_CLASSES);
+      expect(button).toBeDisabled();
+    });
+  });
 });
