bdb6d55f368bf3519d049d64e02cfc398148ed5c
¡Duque <atanaelleonardo@gmail.com>
2026-01-21 22:12:30 -0300

    feat(ui): add argTypes to InputText stories for InputText component

--- src/components/InputText/InputText.stories.tsx
+++ src/components/InputText/InputText.stories.tsx
@@ -12,7 +12,37 @@ const meta: Meta<typeof InputText> = {
     ),
   ],
   tags: ['autodocs'],
-  argTypes: {},
+  argTypes: {
+    type: {
+      control: 'select',
+      options: ['text', 'password', 'email', 'tel', 'url', 'search'],
+      description: 'Este é o tipo do input',
+    },
+    labelText: {
+      control: 'text',
+      description: 'Label text do input',
+    },
+    errorMessage: {
+      control: 'text',
+      description: 'Mensagem de erro ao usuário',
+    },
+    placeholder: {
+      control: 'text',
+      description: 'Exemplo de uso para o input',
+    },
+    required: {
+      control: 'boolean',
+      description: 'O campo é requerido',
+    },
+    disabled: {
+      control: 'boolean',
+      description: 'O campo está desativado',
+    },
+    readOnly: {
+      control: 'boolean',
+      description: 'Apenas leitura',
+    },
+  },
 };
 export default meta;
 
@@ -27,11 +57,27 @@ export const Default: Story = {
     required: true,
     disabled: false,
     readOnly: false,
+    defaultValue: 'Valor padrão do input',
   },
 };
 
 export const withError: Story = {
   args: {
     ...Default.args,
+    errorMessage: 'Esta é a mensagem de erro',
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    ...Default.args,
+    disabled: true,
+  },
+};
+
+export const ReadOnly: Story = {
+  args: {
+    ...Default.args,
+    readOnly: true,
   },
 };
