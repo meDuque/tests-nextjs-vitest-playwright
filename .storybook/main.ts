3b1af449c10de9a1efcdd63851c3561e2e4bcc85
¡Duque <atanaelleonardo@gmail.com>
2026-01-31 03:36:04 -0300

    feat: update Storybook configuration and add TodoForm stories

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,13 @@
-import type { StorybookConfig } from "@storybook/nextjs-vite";
+import type { StorybookConfig } from '@storybook/nextjs-vite';
 
 const config: StorybookConfig = {
   stories: [
-    "../src/**/*.stories.@(js|jsx|ts|tsx)",
-    "../src/**/stories.@(js|jsx|ts|tsx)",
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+    '../src/**/stories.@(js|jsx|ts|tsx)',
   ],
-  addons: ["@storybook/addon-docs"],
-  framework: "@storybook/nextjs-vite",
-  staticDirs: ["..\\public"],
+  addons: ['@storybook/addon-docs', '@storybook/test'],
+  framework: '@storybook/nextjs-vite',
+  staticDirs: ['..\\public'],
   features: {
     backgrounds: false,
   },
