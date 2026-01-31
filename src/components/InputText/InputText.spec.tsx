fe79e141a11b5c7b210ec8a536f0e85adaa1def4
¡Duque <atanaelleonardo@gmail.com>
2026-01-31 02:12:54 -0300

    Lint: Revert import statement in InputText.spec.tsx

--- src/components/InputText/InputText.spec.tsx
+++ src/components/InputText/InputText.spec.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
-import { InputText, type InputTextProps } from '.';
 import userEvent from '@testing-library/user-event';
+import { InputText, type InputTextProps } from '.';
 
 type Props = Partial<InputTextProps>;
 
